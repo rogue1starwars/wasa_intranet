f549662d745e54b90ffd90e3bf0cd813096149e5
Tetsuka <rogue1starwars728@yahoo.co.jp>
2024-02-11 11:27:38 +0900

    deleted unused comment

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -4,15 +4,6 @@ import { fetchMapData } from "@/lib/data";
 export default async function Map({ id }: { id?: string }) {
   const boxData = await fetchMapData();
   if (id) {
-    // Object.keys(boxData).map((data) => {
-    //     if (data.id === id) {
-    //         switch (data.shelf) {
-    //         case 1:
-    //             return <Shelf1 boxData={boxData} id={id}/>;
-    //         }
-    //     }
-
-    // })
     switch (boxData[id].shelf) {
       case 1:
         return (
